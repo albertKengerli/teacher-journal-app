f399d6df921eee50667c0e5b6c18bc43bf9476e9
Albert Kenherli <Albert_Kenherli@epam.com>
2020-02-28 13:20:17 +0300

    feat: create component for showing statistics

---
+++ src/app/components/statistics/statistics-page/statistics-page.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs";
+
+import { Student } from "../../../common/entities/student";
+import { Subject } from "../../../common/entities/subject";
+
+import { StudentService } from "../../../common/services/student.service";
+import { SubjectService } from "../../../common/services/subject.service";
+
+@Component({
+  selector: 'app-statistics-page',
+  templateUrl: './statistics-page.component.html',
+  styleUrls: ['./statistics-page.component.scss']
+})
+export class StatisticsPageComponent implements OnInit, OnDestroy {
+  selectedStudent: Student;
+  selectedSubject: Subject;
+  students: Student[];
+  subjects: Subject[];
+
+  studentSubscription: Subscription;
+  subjectSubscription: Subscription;
+
+  constructor(
+    private studentService: StudentService,
+    private subjectService: SubjectService,
+  ) { }
+
+  ngOnInit(): void {
+    this.studentSubscription = this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+    this.subjectSubscription = this.subjectService.getSubjects()
+      .subscribe(subjects => this.subjects = subjects);
+  }
+
+  selectStudent(student: Student):void {
+    this.selectedStudent = student;
+  }
+
+  selectSubject(subject: Subject): void {
+    this.selectedSubject = subject;
+  }
+
+  ngOnDestroy(): void {
+    this.studentSubscription.unsubscribe();
+    this.subjectSubscription.unsubscribe();
+  }
+
+  test(): void {
+    console.log(this);
+  }
+}
