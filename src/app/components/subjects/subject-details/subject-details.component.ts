944fef30ce1dd456f21ab41512da542aab531342
Albert Kenherli <Albert_Kenherli@epam.com>
2020-03-03 10:19:26 +0300

    feat: make subject-details see table/teacher changes, add save button

--- src/app/components/subjects/subject-details/subject-details.component.ts
+++ src/app/components/subjects/subject-details/subject-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { SubjectService } from '../../../common/services/subject/subject.service';
 
 import { Subject } from '../../../common/entities/subject';
+import { StudentWithGrades } from '../../../common/entities/student';
 
 @Component({
   selector: 'app-subject-details',
@@ -12,18 +13,46 @@ import { Subject } from '../../../common/entities/subject';
 })
 export class SubjectDetailsComponent implements OnInit {
   subject: Subject;
+  teacherChanged: boolean;
+  gradesChanged: boolean;
+  dataToSend: StudentWithGrades[];
 
   constructor(
     private route: ActivatedRoute,
-    private subjectService: SubjectService
+    private router: Router,
+    private subjectService: SubjectService,
   ) { }
 
   ngOnInit(): void {
     this.getSubject();
   }
 
-  getSubject(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.subject = this.subjectService.getSubjectById(id);
+  private getSubject(): void {
+    const name = this.route.snapshot.paramMap.get('name');
+    this.subject = this.subjectService.getSubjectByName(name);
+  }
+
+  saveNewGrades(data: any): void {
+    if (!this.gradesChanged) {
+      this.gradesChanged = true;
+    }
+    this.dataToSend = data;
+  }
+
+  renameTeacher(event: Event): void {
+    if (!this.teacherChanged) {
+      this.teacherChanged = true;
+    }
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.subject.teacher = newValue;
+  }
+
+  save(): void {
+    this.router.navigate(['subjects']);
+  }
+
+  cancel(): void {
+    this.router.navigate(['subjects']);
   }
 }
