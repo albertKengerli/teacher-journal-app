459e91ea8b591476fc13c40e1fe28f7674c1e08b
Albert Kenherli <Albert_Kenherli@epam.com>
2020-03-04 13:53:38 +0300

    refactor: get students and subjects child routes to another file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,64 +1,27 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { StudentsPageComponent } from "./components/students/students-page/students-page.component";
-import { SubjectsPageComponent } from "./components/subjects/subjects-page/subjects-page.component";
-import { StudentsFormComponent } from "./components/students/students-form/students-form.component";
-import { SubjectFormComponent } from "./components/subjects/subject-form/subject-form.component";
-import { SubjectDetailsComponent } from "./components/subjects/subject-details/subject-details.component";
-import { NotFoundComponent } from "./components/misc/not-found/not-found.component";
 import { StatisticsPageComponent } from "./components/statistics/statistics-page/statistics-page.component";
 import { ExportPageComponent } from "./components/export/export-page/export-page.component";
 import { StudentsRootComponent } from "./components/students/students-root/students-root.component";
 import { SubjectsRootComponent } from "./components/subjects/subjects-root/subjects-root.component";
+import { NotFoundComponent } from "./components/misc/not-found/not-found.component";
+
+import { studentsRoutes } from "./modules/students/students.routes";
+import { subjectsRoutes } from "./modules/subjects/subjects.routes";
 
 const routes: Routes = [
   {
     path: "students",
     component: StudentsRootComponent,
     data: { breadcrumb: "Students"},
-    children: [
-      {
-        path: "add",
-        component: StudentsFormComponent,
-        data: { breadcrumb: "Add student"},
-      },
-      {
-        path: "",
-        pathMatch: "full",
-        component: StudentsPageComponent,
-      },
-      {
-        path: "**",
-        component: NotFoundComponent,
-      },
-    ],
+    children: studentsRoutes,
   },
   {
     path: "subjects",
     component: SubjectsRootComponent,
     data: { breadcrumb: "Subjects"},
-    children: [
-      {
-        path: "add",
-        component: SubjectFormComponent,
-        data: { breadcrumb: "Add subject"},
-      },
-      {
-        path: ":name",
-        component: SubjectDetailsComponent,
-        data: { breadcrumb: "Subject page"},
-      },
-      {
-        path: "",
-        pathMatch: "full",
-        component: SubjectsPageComponent,
-      },
-      {
-        path: "**",
-        component: NotFoundComponent,
-      },
-    ],
+    children: subjectsRoutes,
   },
   {
     path: "export",
