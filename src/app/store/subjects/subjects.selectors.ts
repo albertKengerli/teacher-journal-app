45400dc5bf27e7ebf3be34d3d3dc66b3fbd83c59
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2020-03-17 12:46:51 +0300

    feat: create selector getSubjectByName

--- src/app/store/subjects/subjects.selectors.ts
+++ src/app/store/subjects/subjects.selectors.ts
@@ -1,8 +1,15 @@
-import { createFeatureSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 
 import { SubjectsState } from "./subjects.state";
 
 import { entitiesNames } from "../../common/constants/entitiesNames";
 
-/* tslint:disable-next-line:typedef */
+/* tslint:disable:typedef */
 export const getSubjectsState = createFeatureSelector<SubjectsState>(entitiesNames.subjects);
+
+export const getSubjectByName = createSelector(
+  getSubjectsState,
+  (state: SubjectsState, props) => {
+    return state.data.find( subject => subject.name === props.name);
+  }
+);
