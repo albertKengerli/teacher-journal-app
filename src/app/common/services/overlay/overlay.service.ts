ede98442e2588c3efaceb6ccd999769cfdbcd607
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2020-03-17 20:25:51 +0300

    feat: create service for overlays

---
+++ src/app/common/services/overlay/overlay.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+
+import { Overlay, OverlayRef } from "@angular/cdk/overlay";
+import { ComponentPortal } from "@angular/cdk/portal";
+
+import { MatSpinner } from "@angular/material/progress-spinner";
+
+@Injectable({
+  providedIn: "root"
+})
+export class OverlayService {
+  private spinnerOverlay: OverlayRef = this.createSpinner();
+
+  constructor(private overlay: Overlay) { }
+
+  private createSpinner(): OverlayRef {
+    return this.overlay.create({
+      hasBackdrop: true,
+      backdropClass: "spinnerBackdrop",
+      positionStrategy: this.overlay.position()
+        .global()
+        .centerVertically()
+        .centerHorizontally()
+    });
+  }
+
+  public showSpinner(): void {
+    this.spinnerOverlay.attach(new ComponentPortal(MatSpinner));
+  }
+
+  public stopSpinner(): void {
+    this.spinnerOverlay.detach();
+  }
+}
