4d2f71456b2c0560ae834cb5c0c9fe82e4f25029
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2020-03-30 16:55:24 +0300

    refactor: add GradeOperations enum to gradesConstants

--- src/app/common/constants/gradesConstants.ts
+++ src/app/common/constants/gradesConstants.ts
@@ -3,3 +3,10 @@ export enum GradesConstants {
   MinGrade = 0,
   PositivenessBorder = 5,
 }
+
+export enum GradeOperations {
+  Delete = "Delete grade",
+  Post = "Post grade",
+  Update = "Update grade",
+  Remove = "Remove operation"
+}
