7c45068da11755ac01fe2a7e7cd6db421718c7a3
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2020-04-07 16:42:13 +0300

    feat: add dropdown component for statistics

--- src/app/modules/statistics/statistics.module.ts
+++ src/app/modules/statistics/statistics.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from "@angular/common";
 import { SharedModule } from "../shared/shared.module";
 
 import { StatisticsPageComponent } from "../../components/statistics/statistics-page/statistics-page.component";
+import { DropdownPickerComponent } from "../../components/statistics/dropdown-picker/dropdown-picker.component";
 
 @NgModule({
   declarations: [
     StatisticsPageComponent,
+    DropdownPickerComponent,
   ],
   imports: [
     CommonModule,
