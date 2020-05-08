455f3b4a1be4f765403ef89f013ec4fe7936516e
Albert Kenherli <Albert_Kenherli@epam.com>
2020-05-08 21:51:09 +0300

    refactor: make subject table have minimum amount of logic and use services instead

--- src/app/components/subjects/subject-table/subject-table.component.ts
+++ src/app/components/subjects/subject-table/subject-table.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from "@angular/core";
-import { FormControl } from "@angular/forms";
 
 import { MatTableDataSource } from "@angular/material/table";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 
-import { Subscription, Observable } from "rxjs";
+import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
 
 import { DatepickerDialogComponent } from "../../../shared/components/datepicker-dialog/datepicker-dialog.component";
 
 import { SubjectTableDatesService } from "../../../common/services/subject-table/subject-table-dates/subject-table-dates.service";
-import { GradesSenderService } from "../../../common/services/grades-sender/grades-sender.service";
-import { TranslateService } from "@ngx-translate/core";
+import { SubjectTableGradesService } from "../../../common/services/subject-table/subject-table-grades/subject-table-grades.service";
 
 import { Store, select } from "@ngrx/store";
-import { AppState, getEditableGradeIdByProperties } from "../../../store";
+import { AppState } from "../../../store";
 
 import {
   getSubjectTableStudents,
@@ -23,21 +22,13 @@ import {
   getSubjectTableExistingDates,
 } from "../../../store/subjectTable/subjectTable.selectors";
 
-import * as EditableGradesActions from "../../../store/editableGrades/editableGrades.actions";
-
 import { Student } from "../../../common/entities/student";
 import { Subject } from "../../../common/entities/subject";
-import { Grade } from "../../../common/entities/grades";
 import { SubjectTableDateObject } from "../../../common/entities/subjectTable";
-
-import * as GradeUtility from "../../../common/helpers/GradeUtility";
-
-import { GradeOperations } from "../../../common/constants/gradesConstants";
 
 import { subjectTablePaginationStep } from "./subject-table.model";
 import { PaginatorSelection } from "../../../shared/components/paginator/paginator.model";
 import { datepickerDimensions } from "../../../common/constants/dialogDimensions";
-import { filter, take } from "rxjs/operators";
 
 @Component({
   selector: "app-subject-table",
@@ -61,16 +52,13 @@ export class SubjectTableComponent implements OnInit, OnDestroy {
   public columnsNamesList: string[];
   public selectedDates: SubjectTableDateObject[];
 
-  public datePickControl: FormControl = new FormControl(new Date());
-
   public datesQuantity: number;
   public paginationStep: number = subjectTablePaginationStep;
 
   constructor(
     private store: Store<AppState>,
     private tableDatesService: SubjectTableDatesService,
-    private gradesSenderService: GradesSenderService,
-    private translateService: TranslateService,
+    private tableGradesService: SubjectTableGradesService,
     private dialog: MatDialog,
   ) { }
 
@@ -94,78 +82,9 @@ export class SubjectTableComponent implements OnInit, OnDestroy {
     this.existingDates = [...dates];
   }
 
-  private isGradeValidForTable(gradeAsString: string, gradeAsNumber: number): boolean {
-    if ( GradeUtility.isGradeValid(gradeAsNumber) || gradeAsString === "") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  private getGradeOperationForSending(gradeAlreadyExists: boolean, gradeIsDeleted: boolean): string {
-    if (gradeAlreadyExists && !gradeIsDeleted) {
-      return GradeOperations.Update;
-    }
-    if (gradeAlreadyExists && gradeIsDeleted) {
-      return GradeOperations.Delete;
-    }
-    if (!gradeAlreadyExists && !gradeIsDeleted) {
-      return GradeOperations.Post;
-    }
-    if (!gradeAlreadyExists && gradeIsDeleted) {
-      return GradeOperations.RevertOperation;
-    }
-  }
-
-  private cancelGradeChange(gradeInput: HTMLElement): void {
-    gradeInput.textContent = this.editingValue;
-    this.editingValue = null;
-
-    const alertMessage: string = this.translateService.instant("ALERT.SUBJECT_TABLE_GRADE_ERROR");
-    window.alert(alertMessage);
-    throw alertMessage;
-  }
-
-  private getGradeId(studentId: number, subjectId: number, date: number): Observable<number> {
-    return this.store.pipe(select(getEditableGradeIdByProperties, {
-      studentId,
-      subjectId: this.subject.id,
-      date,
-    }));
-  }
-
-  private updateEditableGrades(
-    gradeId: number,
-    studentId: number,
-    subjectId: number,
-    grade: number,
-    date: number,
-    gradeOperation: string
-  ): void {
-    const newGrade: Grade = {
-      id: gradeId,
-      studentId,
-      subjectId,
-      date,
-      grade,
-    };
-
-    switch (gradeOperation) {
-      case GradeOperations.Update:
-        this.store.dispatch(EditableGradesActions.updateEditableGrade({ id: gradeId, newGrade }));
-        break;
-      case GradeOperations.Post:
-        this.store.dispatch(EditableGradesActions.postEditableGrade({ grade: newGrade }));
-        break;
-      default:
-        break;
-    }
-  }
-
   public ngOnInit(): void {
     this.studentsSubscription = this.store.pipe(
       select(getSubjectTableStudents),
-      take(1),
     ).subscribe( students => this.updateDataSource(students));
 
     this.selectedDatesSubscription = this.store.pipe(
@@ -197,44 +116,23 @@ export class SubjectTableComponent implements OnInit, OnDestroy {
 
   public handleGradeChange(studentId: number, date: number, event: Event): void {
     const gradeInput: HTMLElement = event.target as HTMLElement;
+    const changeWasMade: boolean = gradeInput.textContent !== this.editingValue;
 
-    if (gradeInput.textContent !== this.editingValue) {
-      const gradeAsString: string = gradeInput.textContent.trim();
-      const gradeAsNumber: number = gradeAsString === "" ? null : Number(gradeAsString);
+    if (changeWasMade) {
+      const enteredGrade: string = gradeInput.textContent.trim();
 
-      if (!this.isGradeValidForTable(gradeAsString, gradeAsNumber)) {
-        this.cancelGradeChange(gradeInput);
+      try {
+        this.tableGradesService.updateGrade(studentId, this.subject.id, date, enteredGrade);
+        gradeInput.textContent = "";
+        this.gradesChange.emit();
+      } catch (error) {
+        gradeInput.textContent = this.editingValue;
+        window.alert(error);
       }
-
-      let gradeId: number;
-      let gradeAlreadyExists: boolean;
-      const gradeIsDeleted: boolean = gradeAsNumber ? false : true;
-
-      this.getGradeId(studentId, this.subject.id, date)
-        .subscribe( foundGradeId => {
-          gradeId = foundGradeId;
-          gradeAlreadyExists = gradeId ? true : false;
-        });
-
-      if (!gradeId) {
-        gradeId = GradeUtility.generateId(studentId, this.subject.id, date);
-      }
-
-      const gradeOperation: string = this.getGradeOperationForSending(gradeAlreadyExists, gradeIsDeleted);
-
-      if (!gradeIsDeleted) {
-        this.updateEditableGrades(gradeId, studentId, this.subject.id, gradeAsNumber, date, gradeOperation);
-      }
-
-      gradeInput.textContent = gradeAsString;
-      this.editingValue = null;
-
-      this.gradesChange.emit();
-      this.gradesSenderService.prepareGradeForSending(gradeId, gradeOperation);
     }
   }
 
-  public handleEnter(event: KeyboardEvent): void {
+  public handleEnterKey(event: KeyboardEvent): void {
     const target: HTMLElement = event.target as HTMLElement;
     target.blur();
   }
